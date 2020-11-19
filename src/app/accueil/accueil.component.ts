0e6e2e5ba3a7ff452e5b0398a02c65db9f3871c5
jeremylaynaud <65315561+jeremylaynaud@users.noreply.github.com>
2020-11-19 17:41:25 +1100

    Revert "ajout de leaflet et du shadow"

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as L from 'leaflet';
-
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
@@ -12,11 +10,6 @@ export class AccueilComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
-
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'Frugal Map'
-    }).addTo(myfrugalmap);
   }
 
 }
