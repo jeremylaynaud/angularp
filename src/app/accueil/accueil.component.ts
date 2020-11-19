2126a74b99733c4af912b3d27a2f38746e89701e
Laynaud Jeremy <jlaynaud@gmail.com>
2020-11-19 16:51:59 +1100

    ajout de leaflet et du shadow

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as L from 'leaflet';
+
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
@@ -10,6 +12,11 @@ export class AccueilComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Frugal Map'
+    }).addTo(myfrugalmap);
   }
 
 }
